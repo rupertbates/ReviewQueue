6b8396b229b869fd1c7bf86b28da8542198bb5aa
rupertbates <rupert.bates@guardian.co.uk>
2011-02-18 16:25:08 +0000

    first commit

---
+++ src/com/guardian/reviewQueue/activities/MainActivity.scala
@@ -0,0 +1,16 @@
+package com.guardian.reviewQueue.activities
+
+import _root_.android.app.Activity
+import _root_.android.os.Bundle
+import _root_.android.widget.TextView
+import com.guardian.reviewQueue.model.Review
+
+class MainActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(new TextView(this) {
+      val r = new Review()
+      setText(r.name)
+    })
+  }
+}
---
+++ src/com/guardian/reviewQueue/model/Review.scala
@@ -0,0 +1,10 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: rupert bates
+ * Date: 18/02/11
+ * Time: 16:16
+ */
+package com.guardian.reviewQueue.model;
+class Review (id: String){
+  val name = "Hello name"
+}
